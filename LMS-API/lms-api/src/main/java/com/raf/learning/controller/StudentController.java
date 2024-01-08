f51ccb303899495ff465aea8cdbc5b6a807ed91d
Devic Luka <luka.m.devic@gmail.com>
2024-01-08 18:10:24 +0100

    RAF-LMS-31 feature: get, create and delete endpoints

--- LMS-API/lms-api/src/main/java/com/raf/learning/controller/StudentController.java
+++ LMS-API/lms-api/src/main/java/com/raf/learning/controller/StudentController.java
@@ -9,7 +9,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
-
     private final StudentRepository repository;
 
     public StudentController(StudentRepository repository) {
@@ -21,9 +20,27 @@ public class StudentController {
         return repository.findAll();
     }
 
-    @PostMapping
-    public Student createStudent(@RequestBody Student newStudent) {
-        return repository.save(newStudent);
+    @GetMapping("/{id}")
+    public String getStudent(@PathVariable Integer id){
+        return repository.findById(id).isPresent()
+                ? repository.findById(id).get().toString()
+                : String.format("Student with id: %s doesn't exist in the db", id);
     }
 
+    @PostMapping
+    public String createStudent(@RequestBody Student newStudent) {
+        if(newStudent.getId() == null) {
+            return "Provided input doesn't contain id, please add id to your request json";
+        }
+        return repository.save(newStudent).toString();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Integer id){
+      if (repository.findById(id).isEmpty()){
+          return String.format("Student with id: %s doesn't exist in the db", id);
+      }
+      repository.deleteById(id);
+      return String.format("Student with id: %s has been successfully deleted", id);
+    }
 }
