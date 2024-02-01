d0eb6099299914701b52c367f8fa44de0a3a55d2
Devic Luka <luka.m.devic@gmail.com>
2024-02-01 14:37:04 +0100

    LMS-API-31 feature: load test

---
+++ LMS-API/lms-api/src/test/java/com/raf/learning/StudentsE2ETests.java
@@ -0,0 +1,34 @@
+package com.raf.learning;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class StudentsE2ETests {
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+
+    @Test
+    public void testRafStudentsGetApi() {
+        String result;
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://192.168.124.28:8091/api/v1/students"))
+                    .build();
+
+            java.net.http.HttpResponse<String> response = CLIENT.send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
+            result = response.body();
+            System.out.println("### response: \n" + result);
+            assertThat(response.statusCode(), is(equalTo(200)));
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
