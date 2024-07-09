ab02096f0d9f09683cc9d69511c5a99c186790ff
Devic Luka <luka.m.devic@gmail.com>
2024-07-09 22:58:49 +0200

    fix: cleanup

--- src/main/java/com/raf/learning/authorisation/TokenAuthenticationFilter.java
+++ src/main/java/com/raf/learning/authorisation/TokenAuthenticationFilter.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class TokenAuthenticationFilter implements Filter {
-    private final String validToken = "L2aTA643Z0UJ43bIdBymFExVbpqZg7v5QJafYh6KFRjl04eV6w4TtdppkX41hEwo"; // Change this to your valid token
+    private final String validToken = "token-value"; // Change this to your valid token
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
